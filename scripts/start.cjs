2ea9094f76cb133d685460fad0b73c0dab446f70
Jared Grippe <jared@deadlyicon.com>
2022-05-11 13:47:57 -0700

    ./scripts/start.{,c}js

--- scripts/start.cjs
+++ scripts/start.cjs
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
-const Path = require('path')
-import(Path.join(__dirname, 'start.js'))
+
+// using a cjs file here so phusion passenger can require() this file
+
+async function main(){
+  require('dotenv').config()
+  const { default: server } = await import('../index.js')
+  server.start()
+}
+
+main().catch(error => {
+  console.error(error)
+  process.exit(1)
+})
--- scripts/start.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-import dotenv from 'dotenv'
-dotenv.config()
-
-import jlinxHttpServer from '../index.js'
-jlinxHttpServer.start()
